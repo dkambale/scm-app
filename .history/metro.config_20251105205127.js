081e61462a3f745ed4a0206d3e3f9bccfa21388a
vijayholve <vijaygholve77v@gmail.com>
2025-11-06 16:46:51 +0530

    UP

--- .history/metro.config_20251105205127.js
+++
@@ -1,5 +0,0 @@
-const { getDefaultConfig } = require('expo/metro-config');
-const { withNativeWind } = require('nativewind/metro');
-const config = getDefaultConfig(__dirname);
-module.exports = config; // MUST use module.exports
-module.exports = withNativeWind(config, { input: './global.css' });
